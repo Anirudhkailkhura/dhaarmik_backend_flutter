6cef7bab84aa80efb64b894b311450a0fefef3bf
AnirudhKailkhura <anirudhkailkhura@gmail.com>
2024-01-20 22:17:08 +0530

    add logger

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -18,6 +18,7 @@ module.exports = {
       await newUser.save();
       res.status(201).json({ message: "User successfully created" });
     } catch (error) {
+      console.log(error);
       res
         .status(500)
         .json({ error: "Internal Server Error", details: error.message });
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const productRoute = require("./routers/products");
 const authRoute = require("./routers/auth");
 const userRoute = require("./routers/users");
@@ -12,7 +13,6 @@ const ordersRoute = require("./routers/users");
 
 const port = 3000;
 
-
 dotenv.config();
 
 mongoose
@@ -20,6 +20,7 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
+app.use(morgan("combined"));
 app.use(cors())
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
