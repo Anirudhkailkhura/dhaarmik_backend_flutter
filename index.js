496ba5b3dcea597a6a5f2c7587dcb5e0ed4a96fa
AnirudhKailkhura <anirudhkailkhura@gmail.com>
2024-01-20 21:50:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require('bcrypt');
+const cors = require('cors');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
@@ -19,6 +20,7 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
 
+app.use(cors())
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
